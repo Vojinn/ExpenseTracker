db843b4a01cc4bf34df7fc659c0dee575ddc062e
Vojin <dvojinovic38@gmail.com>
2023-02-28 22:57:33 +0100

    added transaction row and model

---
+++ ExpenseTracker/TransactionModel.swift
@@ -0,0 +1,37 @@
+//
+//  TransactionModel.swift
+//  ExpenseTracker
+//
+//  Created by Dusan Vojinovic on 27.2.23..
+//
+
+import Foundation
+
+struct Transaction: Identifiable {
+    let id: Int
+    let date: String
+    let institution: String
+    let account: String
+    let merchant: String
+    let amount: Double
+    let type: TransactionType.RawValue
+    let categoryId: Int
+    var category: String
+    let isPending: Bool
+    var isTransfer: Bool
+    var isExpense: Bool
+    var isEdited: Bool
+    
+    var dateParsed: Date {
+        date.dateParsed()
+    }
+    
+    var signedAmount: Double {
+        return type == TransactionType.credit.rawValue ? amount : -amount
+    }
+}
+
+enum TransactionType: String {
+    case debit = "debit"
+    case credit = "credit"
+}
